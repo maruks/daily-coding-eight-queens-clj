d98af33bf084c6cec6de06bac2860b7180708bfb
Maris Orbidans <maris.orbidans@onthemarket.com>
2019-05-13 21:17:39 +0100

    add tests

--- src/eight_queens/core.clj
+++ src/eight_queens/core.clj
@@ -8,25 +8,26 @@
    (into #{})))
 
 (defn- attack-positions [x y size]
-  (into #{}
-        (concat (for [xp (range size)] [xp y])
-                (for [yp (range size)] [x yp])
-                (for [xp (range size)
-                      yp (range size) :when (or (= (+ xp yp) (+ x y)) (= (- xp yp) (- x y)))] [xp yp]))))
+  (->>
+   (concat (for [xp (range size)] [xp y])
+           (for [yp (range size)] [x yp])
+           (for [xp (range size)
+                 yp (range size) :when (or (= (+ xp yp) (+ x y)) (= (- xp yp) (- x y)))] [xp yp]))
+   (into #{})))
 
 (defn- queens [valid-positions size x]
   (cond (= size x) 1
         (empty? valid-positions) 0
         :else  (let [place-pos (for [yp (range size) :when (valid-positions [x yp])] [x yp])]
                  (transduce
-                         (map (fn [[x y]]
-                                (queens
-                                 (difference valid-positions (attack-positions x y size))
-                                 size
-                                 (inc x)))) + 0 place-pos))))
-
-(defn eight-queens []
-  (n-queens 8))
+                  (map (fn [[x y]]
+                         (queens
+                          (difference valid-positions (attack-positions x y size))
+                          size
+                          (inc x)))) + 0 place-pos))))
 
 (defn n-queens [n]
   (queens (all-pos n) n 0))
+
+(defn eight-queens []
+  (n-queens 8))
--- test/eight_queens/core_test.clj
+++ test/eight_queens/core_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [eight-queens.core :refer :all]))
 
-(deftest a-test
+(deftest n-queens-test
   (is (= 2 (n-queens 4)))
   (is (= 10 (n-queens 5)))
   (is (= 4 (n-queens 6)))
